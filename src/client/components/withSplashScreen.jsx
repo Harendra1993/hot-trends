5c66b010f75eb88439077ef25df764af8c3c7789
testuser <testuser@example.com>
2019-05-27 02:16:14 -0400

    refactor code

--- src/client/components/withSplashScreen.jsx
+++ src/client/components/withSplashScreen.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { getTrends } from '../helpers/requests';
-import './splash-screen.css';
+import '../App.css';
 
 function LoadingMessage() {
     return (
